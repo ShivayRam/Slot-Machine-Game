70c1e165a8ca6fa4c3c50c9718fff4fc53e5b201
ShivayRam <2582526@students.wits.ac.za>
2025-03-09 19:57:30 +0200

    Finished project

--- project.js
+++ project.js
@@ -11,11 +11,18 @@ const prompt = require("prompt-sync")(); // Import prompt-sync, imports it into 
 const ROWS = 3; // Number of rows in the slot machine
 const COLS = 3; // Number of columns in the slot machine
 
-const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
+const SYMBOLS_COUNT = { //dont need the "" around the keys in JS
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+
+const SYMBOL_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2,
 }
 
 
@@ -59,6 +66,103 @@ const getBet = (balance, lines) => {
 
 }
 
-let balance = deposit(); // Calls the deposit function and stores the return value in the variable balance
-const numOfLines = getNumOfLines(); // Calls the getNumOfLines function and stores the return value in the variable numOfLines
-const bet = getBet(balance, numOfLines); // Calls the getBet function and stores the return value in the variable bet
+const spin = () => {
+    const symbols = [];
+    for (const[symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [];  //arrays inside represent our columns
+
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]); //push an empty array into our reels array
+        const reelSymbols = [...symbols]; //copy of symbols array
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length); //generates a random number between 0 and the length of the symbols array - 1.
+            const selectedSymbol = reelSymbols[randomIndex]; //select the element/symbol from our reelssymbol array
+            reels[i].push(selectedSymbol); //push the selected symbol into our reels array
+            reelSymbols.splice(randomIndex, 1); //remove the selected symbol from our reelSymbols array
+        }
+    }
+
+    return reels;
+}
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+       let rowString = "";
+       for (const [i, symbol] of row.entries()) {
+           rowString += symbol;
+           if (i != row.length - 1) {
+                rowString += " | ";
+           }
+       }
+       console.log(rowString);
+    }
+}
+
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+
+    return winnings;
+}
+
+const game = () => {
+   
+    let balance = deposit();
+  
+    while (true) {
+      console.log("You have a balance of $" + balance);
+      const numberOfLines = getNumOfLines();
+      const bet = getBet(balance, numberOfLines);
+      balance -= bet * numberOfLines;
+      const reels = spin();
+      const rows = transpose(reels);
+      printRows(rows);
+      const winnings = getWinnings(rows, bet, numberOfLines);
+      balance += winnings;
+      console.log("You won, $" + winnings.toString());
+  
+      if (balance <= 0) {
+        console.log("You ran out of money!");
+        break;
+      }
+  
+      const playAgain = prompt("Do you want to play again (y/n)? ");
+  
+      if (playAgain != "y") break;
+    }
+  };
+  
+  game();
